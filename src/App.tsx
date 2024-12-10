d43b25619ba9ba93f24cc61c15ead7fdd31aefb8
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-12-10 14:09:13 -0300

    chore: create base project + create theme colors

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Encontre o cafe perfeito para qualquer hora do dia</h1>
+      <h2>
+        Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+      </h2>
+    </>
+  );
+}
+
+export default App;
